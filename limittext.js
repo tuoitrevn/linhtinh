a0106bb03bc8fcf9b5f8f1b2c7e1d88b319b8035
tuoitrevn <vanthoang303@gmail.com>
2017-01-22 12:06:34 +0700

    Create limittext.js

---
+++ limittext.js
@@ -0,0 +1,17 @@
+$(function(){
+    $.fn.limit = function($n){
+        this.each(function(){
+            var allText   = $(this).html();
+            var tLength   = $(this).html().length;
+            var startText = allText.slice(0,$n);
+            if(tLength >= $n){
+                $(this).html(startText+'...');
+            }else {
+                $(this).html(startText);
+            };
+        });
+        
+        return this;
+    }
+    $('h2.post-content').limit(120);
+});
