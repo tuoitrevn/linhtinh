7d2d14f8b195069eaf79cb0e4a2d5dc5ee2a09e5
tuoitrevn <vanthoang303@gmail.com>
2017-02-01 01:49:24 +0700

    Update and rename starttext-v1.02.js to starttext-v1.03.js

--- starttext-v1.03.js
+++ starttext-v1.03.js
@@ -15,3 +15,20 @@ $(function(){
     }
     $('.blog_message h2.post-content').limit(150);
 });
+$(function(){
+    $.fn.limit = function($n){
+        this.each(function(){
+            var allText   = $(this).html();
+            var tLength   = $(this).html().length;
+            var startText = allText.slice(0,$n);
+            if(tLength >= $n){
+                $(this).html(startText+'...');
+            }else {
+                $(this).html(startText);
+            };
+        });
+        
+        return this;
+    }
+    $('.recent-des-v3').limit(150);
+});
