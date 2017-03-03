44b84fc3a7c9bd401fb4116b73bb397f60d7a1a1
tuoitrevn <vanthoang303@gmail.com>
2017-03-03 23:59:23 +0700

    Update and rename headroom3.js to headroom4.js

--- headroom4.js
+++ headroom4.js
@@ -1,3 +1,21 @@
+$(function(){
+    $.fn.limit = function($n){
+        this.each(function(){
+            var allText   = $(this).html();
+            var tLength   = $(this).html().length;
+            var startText = allText.slice(0,$n);
+            if(tLength >= $n){
+                $(this).html(startText+'...');
+            }else {
+                $(this).html(startText);
+            };
+        });
+        
+        return this;
+    }
+    $('.blog_message h2.post-content').limit(130);
+});
+
 /*
     Version 1.3.2
     The MIT License (MIT)
@@ -86,21 +104,3 @@
        });
    }); // end of document ready
 })(jQuery); // end of jQuery name space
-
-$(function(){
-    $.fn.limit = function($n){
-        this.each(function(){
-            var allText   = $(this).html();
-            var tLength   = $(this).html().length;
-            var startText = allText.slice(0,$n);
-            if(tLength >= $n){
-                $(this).html(startText+'...');
-            }else {
-                $(this).html(startText);
-            };
-        });
-        
-        return this;
-    }
-    $('.blog_message h2.post-content').limit(130);
-});
