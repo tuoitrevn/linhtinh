18a70581d3400021df712fa4374cc183f3637814
tuoitrevn <vanthoang303@gmail.com>
2017-03-03 22:05:33 +0700

    Create headroomv1.js

---
+++ headroomv1.js
@@ -0,0 +1,19 @@
+(function ($) {
+   $(function () {
+       $("#header").headroom({
+           // vertical offset in px before element is first unpinned
+           offset : 100,
+           // or you can specify tolerance individually for up/down scroll
+           tolerance : {
+               up : 20,
+               down : 20
+           },           classes : {               // when element is initialised
+               initial : "headroom",
+               // when scrolling up
+               pinned : "headroom--pinned",
+               // when scrolling down
+               unpinned : "headroom--unpinned",
+           }
+       });
+   }); // end of document ready
+})(jQuery); // end of jQuery name space
