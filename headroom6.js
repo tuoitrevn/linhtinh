54ab0ebc2017cd81fe97c42dd99a8bb78976a193
tuoitrevn <vanthoang303@gmail.com>
2017-03-04 00:28:51 +0700

    Update and rename headroom5.js to headroom6.js

--- headroom6.js
+++ headroom6.js
@@ -15,28 +15,6 @@ $(function(){
     }
     $('.blog_message h2.post-content').limit(130);
 });
-jQuery(document).ready(function() {
-	jQuery('.ani').addClass("hidden").viewportChecker({
-	    classToAdd: 'visible animated fadeIn', // Class to add to the elements when they are visible
-	    offset: 100    
-	   });   
-}); 
-/*
-    Version 1.3.2
-    The MIT License (MIT)
-
-    Copyright (c) 2014 Dirk Groenen
-
-    Permission is hereby granted, free of charge, to any person obtaining a copy of
-    this software and associated documentation files (the "Software"), to deal in
-    the Software without restriction, including without limitation the rights to
-    use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-    the Software, and to permit persons to whom the Software is furnished to do so,
-    subject to the following conditions:
-
-    The above copyright notice and this permission notice shall be included in all
-    copies or substantial portions of the Software.
-*/
 
 (function($){
     $.fn.viewportChecker = function(useroptions){
